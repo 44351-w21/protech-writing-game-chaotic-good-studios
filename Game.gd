7b1fa9653d862899065875ce2d01cfe268d2e44a
David Angelo <54418832+empCarnivorePewPew@users.noreply.github.com>
2021-04-01 17:35:28 -0500

    2Players w Movement 2out proper assets

--- Game.gd
+++ Game.gd
@@ -1,16 +1,72 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
+onready var cam = $GameCam
+onready var p1 = $Player1
+onready var p2 = $Player2
+var rng = RandomNumberGenerator.new()
+var nextplayer = ['','Player 2','Player 1']
+var currentPlayerNum = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rng.randomize()
+	move_camera(p1)
+	GameState.currentPlayer = p1
+	GameState.currentPlayerLabel = "Player 1"
+	update_label()
+	
+
+
+# moves camera to parent
+
+func move_camera(p):
+	cam.get_parent().remove_child(cam)
+	p.add_child(cam)
+
+	
+
+
+func update_label():
+	#plabel.text = GameState.currentPlayerLabel
+	return
+
+func update_spaceLabel(space):
+	#spaceLabel.text = str(space)	
+	return
+
+func _on_MoveButton_pressed():
+	rng.randomize()
+	#moveBtn.disabled = true
+	GameState.currentPlayer.move(rng.randi_range(1,6))
+	yield(GameState.currentPlayer, 'movedone')
+	$CanvasLayer/Button.show()
+	#moveBtn.visible = false
+	#endBtn.visible = true
+
+
+func _on_EndTurn_pressed():
+	$HUD/TurnSwitch/Label.text=nextplayer[currentPlayerNum]+"'s turn"
+	$HUD/TurnSwitch.visible = true
+
+func _on_Button_pressed():
+	$CanvasLayer/Button.hide()
+	GameState.currentPlayerLabel=nextplayer[currentPlayerNum]
+	match currentPlayerNum:
+		1:
+			GameState.currentPlayer=p2
+			currentPlayerNum=2
+		2:
+			GameState.currentPlayer=p1
+			currentPlayerNum=1
+	update_spaceLabel(GameState.currentPlayer.space)
+	update_label()
+	
+	move_camera(GameState.currentPlayer)
+	_on_MoveButton_pressed()
+	
+	#moveBtn.show()
+	#endBtn.hide()
+	#moveBtn.disabled=false
+	
+	#$HUD/TurnSwitch.visible=false
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- Player.gd
+++ Player.gd
@@ -1,16 +1,43 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var sprite = $Sprite
+export var player = 0;
+onready var tween = $Tween
+var sprites = [preload("res://Assets/Players/PNG/PiecesBlack/pieceBlack_border04.png"), preload("res://Assets/Players/PNG/PiecesBlue/pieceBlue_border06.png")]
+var space = 0
+var dir = Vector2.RIGHT
+var speed = 2
+var tilesize = 64
+var squaresH=18-1
+var squaresV=14-1
+var sumHV=squaresH+squaresV
+var sumHVH=2*squaresH+squaresV
+
+signal movedone
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position.x = tilesize/2
+	position.y = tilesize/2
+	sprite.texture=sprites[player]
 
+func move(spaces):
+	for n in spaces:
+		movespace()
+		yield(tween, "tween_completed")
+		GameState.update_spaceLabel(space)
+	emit_signal("movedone")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func movespace():
+	match space:
+		0: dir = Vector2.RIGHT
+		squaresH: dir = Vector2.DOWN
+		sumHV: dir = Vector2.LEFT
+		sumHVH: dir = Vector2.UP
+	space = (space + 1) % (sumHVH+squaresV)
+	tween.interpolate_property(self, "position", position,
+	position + dir * tilesize, 1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+	
